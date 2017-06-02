d6550dd5e54b89d1665587a48d80e9a2c7aea240
yubo <yubo@133.cn>
2017-06-02 14:59:08 +0800

    add modal

--- TitleBar.js
+++ TitleBar.js
@@ -7,6 +7,7 @@ import {
   Dimensions,
   TouchableOpacity,
   Modal,
+  Button,
 } from 'react-native';
 
 var { width, height } = Dimensions.get('window');
@@ -16,21 +17,12 @@ export default class TitleBar extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      modalVisible: false,
-    }
   }
 
   render() {
     return (
       <View style={styles.titleBarContainer}>
-        <Modal
-          animationType={"slide"}
-          transparent={true}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {alert("Modal has been closed.")}}>
-          <Text>Hello World!</Text>
-        </Modal>
+        <CustomModal ref="modal"/>
         <View style={styles.titleBarTextContainer}>
           <Text style={styles.title}>微信</Text>
         </View>
@@ -53,10 +45,69 @@ export default class TitleBar extends Component {
   }
 
   handleSearchClick = () => {
-    this.setState({modalVisible: true});
+    this.refs.modal.openModal();
   }
 
 }
+
+class CustomModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+    }
+  }
+  render() {
+    return (
+      <Modal
+        animationType={"fade"}
+        transparent={true}
+        visible={this.state.modalVisible}
+        onRequestClose={() => {alert("Modal has been closed.")}}>
+        <View style={modalStyle.container}>
+          <View style={modalStyle.content}>
+            <Text>Hello World! This is a Modal!</Text>
+            <Button
+              style={{marginTop: 20}}
+              title={"Close"}
+              onPress={() => {this.setState({modalVisible: false})}} />
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+  closeModel = () => {
+    this.setState({modalVisible: false});
+  }
+  openModal() {
+    this.setState({modalVisible: true});
+  }
+}
+
+const modalStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
+  content: {
+    width: width - 40,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#FFFFFF',
+    height: 100,
+    borderRadius: 5,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  }
+});
 
 const styles = StyleSheet.create({
   titleBarContainer: {
