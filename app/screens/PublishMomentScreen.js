e5a498fa0de3e2bd549a57ea90389b9ed6e7fad4
admin <admin@admins-mac-pro.ztgame.com>
2018-12-20 17:20:36 +0800

    测试websocket OK, 测试代码已注释

--- app/screens/PublishMomentScreen.js
+++ app/screens/PublishMomentScreen.js
@@ -131,6 +131,30 @@ export default class PublishMomentScreen extends Component {
     });
 
 
+    // var ws = new WebSocket("ws://192.168.99.89:8080/ws/join?uname=appuser");
+
+    // ws.onopen = () => {
+    //   // connection opened
+    //   ws.send("something"); // send a message
+    // };
+
+    // ws.onmessage = e => {
+    //   // a message was received
+    //   console.log(e.data);
+    // };
+
+    // ws.onerror = e => {
+    //   // an error occurred
+    //   console.log(e.message);
+    // };
+
+    // ws.onclose = e => {
+    //   // connection closed
+    //   console.log(e.code, e.reason);
+    // };
+    
+
+
   }
 
   renderImageItem(item, index) {
